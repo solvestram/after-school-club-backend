52c87f616c0a13a1e542f8c05e2b86b703b6c597
Siddikjon <66442163+Siddikjon684@users.noreply.github.com>
2022-01-31 16:18:30 +0000

    added server listener

--- server.js
+++ server.js
@@ -6,3 +6,7 @@ server.use((req, res, next) => {
     console.log(req.method + " " + req.url)
     next();
 });
+
+// Start the server listening on port 8080
+server.listen(8080);
+console.log('Node server is running on port 8080');
