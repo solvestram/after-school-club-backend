1095acd3c9e14587b690ddad959caa12387a51c9
Siddikjon <66442163+Siddikjon684@users.noreply.github.com>
2022-02-01 18:28:13 +0000

    added necessary handlers without functionality

--- server.js
+++ server.js
@@ -3,8 +3,19 @@ const server = express();
 
 // Logging
 server.use((req, res, next) => {
-    console.log(req.method + " " + req.url)
+    console.log(req.method + " " + req.url);
     next();
+});
+
+// Handlers
+server.get('/collections/lessons', (req, res) => {
+    res.send('Received GET request to show information of all lessons');
+});
+server.post('/collections/orders', (req, res) => {
+    res.send('Received order information as a POST request. POST request body: ' + req.body );
+});
+server.put('/collections/lessons/:lesson_id', (req, res) => {
+    res.send(`Received PUT request to change space count of lesson ${req.params.lesson_id} to ${req.body}`);
 });
 
 // Start the server listening on port 8080
